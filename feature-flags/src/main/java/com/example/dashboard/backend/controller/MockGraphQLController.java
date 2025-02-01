bf7bb6aeefa2426122f91781d8b662c88e60a883
Fynn Ruehlemann <fynnruehlemann@gmail.com>
2025-02-01 21:05:08 +0100

    Feature Flag v2

--- feature-flags/src/main/java/com/example/dashboard/backend/controller/MockGraphQLController.java
+++ feature-flags/src/main/java/com/example/dashboard/backend/controller/MockGraphQLController.java
@@ -5,28 +5,43 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class MockGraphQLController {
 
-    private Feature dashboardFeature;
+    private Map<String, Feature> features;
 
     public MockGraphQLController() {
-        this.dashboardFeature = new Feature("dashboard-addition", true);
+        this.features = new HashMap<>();
+        this.features.put("dashboard-addition", new Feature("dashboard-addition", true));
+        this.features.put("teacher-dashboard", new Feature("teacher-dashboard", true));
+        this.features.put("bsp-feature1", new Feature("bsp-feature1", true));
+        this.features.put("bsp-feature2", new Feature("bsp-feature2", true));
+        this.features.put("bsp-feature3", new Feature("bsp-feature3", true));
     }
 
     @QueryMapping
-    public Feature getDashboardFeature() {
-        return dashboardFeature;
+    public Feature getFeature(@Argument String name) {
+        return features.get(name);
+    }
+
+    @QueryMapping
+    public List<Feature> getAllFeatures() {
+        return new ArrayList<>(features.values());
     }
 
     @MutationMapping
-    public Feature setDashboardFeature(@Argument FeatureInput feature) {
-        Feature f = new Feature(feature.name, feature.status);
-        this.dashboardFeature = f;
+    public Feature setFeature(@Argument FeatureInput feature) {
+        Feature f = new Feature(feature.name(), feature.status());
+        features.put(feature.name(), f);
 
-        System.out.println(dashboardFeature.status);
+        System.out.println("Status aktualisiert: " + f.name() + " -> " + f.status());
 
-        return this.dashboardFeature;
+        return f;
     }
 
     public record Feature(String name, boolean status) {}
